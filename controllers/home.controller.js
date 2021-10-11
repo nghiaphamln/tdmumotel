47c810f715993677202a5ed961fc014fed518cff
Minh Nghĩa <nghiaphamln3@gmail.com>
2021-10-11 11:13:57 +0700

    add active feature of navbar and edit content in about

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,7 +1,7 @@
 class HomeController {
     static index (req, res) {
         try {
-            res.render('index', {title: 'Trang chủ'});
+            res.render('index', {title: 'Trang chủ', page_name: 'index'});
         } catch (exception) {
             res.status(500).send(exception);
         }
@@ -9,14 +9,14 @@ class HomeController {
 
     static login (req, res) {
         try {
-            res.render('login', {title: 'Đăng nhập'});
+            res.render('login', {title: 'Đăng nhập', page_name: 'login'});
         } catch (exception) {
             res.status(500).send(exception);
         }
     }
     static about (req, res) {
         try {
-            res.render('about', {title: 'Giới thiệu'});
+            res.render('about', {title: 'Giới thiệu', page_name: 'about'});
         } catch {
             res.status(500).send(exception);
         }
