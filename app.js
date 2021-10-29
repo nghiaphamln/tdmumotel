3254de32f8ed95e2e9525bf9a6d517e6548d54b9
Minh Nghĩa <nghiaphamln3@gmail.com>
2021-10-29 12:23:19 +0700

    format code :v:

--- app.js
+++ app.js
@@ -62,11 +62,11 @@ app.use('/payment', paymentRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-    res.status(404).render('layout', {page_name: '404'});
+    res.status(404).render('layout', { page_name: '404' });
 })
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
